cbc7602f3f9670c3559b6a19e4c275762e59cafd
PetersonNormil <petersonnormil@gmail.com>
2022-12-15 16:26:16 -0500

    added javascript and css functionality

---
+++ Otter Website/Otter Pages/index.js
@@ -0,0 +1,7 @@
+
+function showOtter() {
+    alert("jaleene is so ass at life");
+    document.getElementById("otterGunPic").style.visibility = "visible";
+    console.log("FINALLY SHOWINg The OTTER MF")
+
+}
