5b5a722c36b64dace9dfbdacbb08a338e95439bc
PetersonNormil <petersonnormil@gmail.com>
2022-12-17 11:33:53 -0500

    generating random pictures based on otter pics fodler

--- Otter Website/Otter Pages/index.js
+++ Otter Website/Otter Pages/index.js
@@ -1,7 +1,24 @@
+OtterPicPrefix = "Otter Pics\\OtterPic"; //every otter pic name starts with this string
+OtterPicExt = ".jpg"; //every otter pic is a jpg file, we add this at the end of the string 
+
+otterPicIndex = 1; //this number is what differentiates each picture, we will pick a random number, we are making it 1 by default 
+amtOfOtterPics = 3;//How many otter pics are in the otter pages? 
 
 function showOtter() {
-    alert("jaleene is so ass at life");
-    document.getElementById("otterGunPic").style.visibility = "visible";
-    console.log("FINALLY SHOWINg The OTTER MF")
 
+    //generate a random index based on size of otter pic folder
+    otterPicIndex = getRandomInt(0, amtOfOtterPics);
+
+    //set up the name of the URL of the img, using its index 
+    document.getElementById("otterPicId").src = OtterPicPrefix + otterPicIndex.toString() + OtterPicExt;
+
+    //shows image
+    document.getElementById("otterPicId").style.visibility = "visible";
+
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
